4dd888b1abc0a692caaf4711a77575c4b6e19440
Stephanie <stran40@myseneca.ca>
2018-01-06 00:14:05 -0500

    Corrected pathway of server

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ var path = require("path");
 var HTTP_PORT = process.env.PORT || 8085;
 
 // setup the static folder 
-app.use(express.static(path.resolve(__dirname + './public')));
+app.use(express.static(path.resolve(__dirname + '/public')));
 
 // handle "404" errors
 app.use((req, res) => {
-    res.sendFile(path.join(__dirname + "/../public/index.html"));
+    res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
 // Start the server
